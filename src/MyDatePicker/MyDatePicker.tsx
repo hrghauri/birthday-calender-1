3a3caf7f8b44cd8d02d885090334bb97213cb2f6
Haris Ghauri <hrghauri@gmail.com>
2023-01-15 18:49:00 -0500

    second commit.

--- src/MyDatePicker/MyDatePicker.tsx
+++ src/MyDatePicker/MyDatePicker.tsx
@@ -1,41 +1,15 @@
-// import React from 'react';
-
-// function Calender(props: any) {
-//     return (
-//         <div>
-
-//         </div>
-//     );
-// }
-
-// export default Calender;
-
 import * as React from 'react';
 import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 
 export default function MyDatePicker(props: any) {
-    // const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'));
-    console.log(dayjs().year());
-
 
     return (
-        // <LocalizationProvider dateAdapter={AdapterDayjs}>
-        //     <DatePicker
-        //         label="Basic example"
-        //          openTo="year"
-        //         value={props}
-        //         onChange={(newValue) => {
-        //             console.log(newValue);
-        //         }}
-        //         renderInput={(params: any) => <TextField {...params} />}
-        //     />
-        // </LocalizationProvider>
+
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <StaticDatePicker
                 displayStaticWrapperAs='desktop'
